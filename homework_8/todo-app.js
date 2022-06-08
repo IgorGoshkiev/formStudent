d37c3edf1be77a674ac7ffebaef399810c2aa40e
Игорь Гошкиев <igor.goshkiev@yandex.ru>
2022-06-08 14:53:20 +0300

    Task-8 ver1.3

--- homework_8/todo-app.js
+++ homework_8/todo-app.js
@@ -1,9 +1,13 @@
 (function () {
-    const sessionDict = {
-        'keyMy': [],
-        'keyDad': [],
-        'keyMom': [],
-    };
+
+    let todosArray = [];
+
+    function updateLocalStorage(inkey, todosArray) {
+        console.log('updatelocalstorage', todosArray);
+        localStorage.removeItem(inkey);
+        localStorage.setItem(inkey, JSON.stringify(todosArray));
+    }
+
     // создаем и возвращаем заголовок приложения
     function createAppTitle(title) {
         let appTitle = document.createElement('h2');
@@ -81,130 +85,60 @@
         }
     }
 
-    function addValueToDict(inValue, inKey, dict) {
-        /**
-        *  функция для добавления значения в словарь по ключу
-        * @function addValueToDict
-    
-        * @param  {String} inValue  значение для добавления в словарь.
-        * @param  {String} inKey   ключ словаря, по которому будем добавлять.
-        * @param  {String} dict    словарь.
-        */
-        for (let key in dict) {
-            if (key === inKey) {
-                dict[key].push(inValue);
-            }
-        }
-    }
 
-
-    function readToDict(inKey, invalue, dict) {
-        /**
-        *  читаем и возвращаем список значений по ключу из словаря
-        * @function readToDict
-    
-        * @param  {String} inValue  значение для споиска нужно элемента в словаре.
-        * @param  {String} inKey   ключ словаря, по которому будем определять.
-        * @param  {String} dict    словарь.
-        * @return {String}         возвращаем спискок по ключу
-        */
-        for (let key in dict) {
-            if (key === inKey) {
-                for (let value of dict[key]) {
-                    if (value === invalue) {
-                        console.log('значение =', dict[key]);
-                        return dict[key];
-                    } else {
-                        return false;
-                    }
-                }
-            }
-        }
-    }
-
-    function removeFromLocalstorage(inkey, invalue) {
-        /**
-        *  функция для удаления нужного значения из localstorage
-        * @function removeFromLocalstorage
-    
-        * @param  {String} inKey   ключ  по которому будем искать.
-        * @param  {String} invalue    значение, которое будем искать в localstorage и сравнивать с тем , что есть в списке .
-        */
-        // for (let pairKeyValue of Object.entries(localStorage)) {
-        //     if (inkey === pairKeyValue[0]) {
-        //         localStorage.removeItem(pairKeyValue[0]);
-        //     }
-        // }
-
-        let foundValueList = readToDict(inkey = inkey, invalue = invalue, dict = sessionDict);
-        console.log('словарь1 =', sessionDict);
-        console.log('считанной значени =', foundValueList);
-        if (foundValueList) {
-            let foundIndex = foundValueList.indexOf(invalue);
-            if (foundIndex === -1) {
-                console.log('Индекс не найден =', foundIndex);
-            } else {
-                console.log('Индекс найден =', foundIndex);
-                foundValueList.splice(foundIndex, 1);
-                localStorage.setItem(inkey, JSON.stringify(sessionDict[inkey]));
-            }
-        }
-
-
-
-    }
-
-    function buttonAddRemove(todoItem, inkey) {
+    function buttonAddRemove(todoItem, inkey, todosArray) {
         /**
         *  функция обработчик для кнопок готово и удалить
         * @function buttonAddRemove
     
-        * @param  {String} todoItem  контейнер li .
-        * @param  {String} inkey    ключ, который будем передавать в функцию удаления removeFromLocalstorage.
+        * @param  {String} todoItem  контейнер li.
+        * @param  {String} inkey    ключ, который будем передавать в функцию updateLocalStorage.
+        *  @param  {String} todosArray  массив со значениями.
         */
-        // добавляем обработчики на кнопки
         todoItem.doneButton.addEventListener('click', function () {
             // спомощью функции togggle красим цветом классом из бутстрапа
             todoItem.item.classList.toggle('list-group-item-success');
         });
 
         todoItem.deleteButton.addEventListener('click', function () {
-            // функция confirm встроена в браузер и вернет true если позьзователь согласиться 
             if (confirm('Вы уверены?')) {
-                let delLiText = todoItem.item.querySelector('span');
-                //console.log('for remove', delLiText.textContent);
-                removeFromLocalstorage(inkey, delLiText.textContent);
-                todoItem.item.remove();
+                let inputText = todoItem.item.querySelector('span');
+                console.log('inputText for del =', inputText.textContent);
+                let foundIndex = todosArray.indexOf(inputText.textContent);
+                if (foundIndex === -1) {
+                    console.log('Индекс не найден =', foundIndex);
+                } else {
+                    console.log('Индекс найден =', foundIndex);
+                    todosArray.splice(foundIndex, 1);
+                    updateLocalStorage(inkey, todosArray);
+                    todoItem.item.remove();
+                }
             }
         });
     }
 
-    function createTodoApp(container, title = 'Список дел', inkey) {
 
+    function createTodoApp(container, title = 'Список дел', inkey) {
         let todoAppTitle = createAppTitle(title);
         let todoItemForm = createTodoItemForm();
         let todoList = createTodoList();
-
-        if (localStorage.getItem(inkey) !== null) {
-            for (let pairKeyValue of Object.entries(localStorage)) {
-                if (inkey === pairKeyValue[0]) {
-                    let restorTodoItem = JSON.parse(localStorage.getItem(pairKeyValue[0]));
-                    for (let i = 0; i < restorTodoItem.length; i++) {
-                        let todoItem2 = createTodoItem(restorTodoItem[i]);
-                        todoList.append(todoItem2.item);
-                        buttonAddRemove(todoItem2, inkey);
-                    }
-
-                }
-            }
-        }
-
 
         container.append(todoAppTitle);
         container.append(todoItemForm.form);
         container.append(todoList);
 
+        let savedItems = JSON.parse(localStorage.getItem(inkey));
+        console.log('savedItems', savedItems);
+        if (savedItems != null) {
+            for (element of savedItems) {
+                console.log('element', element);
+                let todoItem2 = createTodoItem(element);
+                todoList.append(todoItem2.item);
 
+                todosArray.push(element);
+                buttonAddRemove(todoItem2, inkey, todosArray);
+            }
+        }
         // браузер создает событие submit на форме (для создания дела)
         todoItemForm.form.addEventListener('submit', function (e) {
             // что бы предотвратить стандартное действие браузера 
@@ -214,19 +148,17 @@
             if (!todoItemForm.input.value) {
                 return;
             }
-            // заполняем словарь новыми значениями
-            addValueToDict(todoItemForm.input.value, inkey, sessionDict);
-            console.log('словарь3 =', sessionDict);
-            localStorage.setItem(inkey, JSON.stringify(sessionDict[inkey]));
-
-            // console.log('данные словаря', localStorage.getItem([key][1]));
-
-
             let todoItem = createTodoItem(todoItemForm.input.value);
             // добавляем в список новое дело с названием из поля для ввода
             todoList.append(todoItem.item);
-            buttonAddRemove(todoItem, inkey);
 
+            let inputText = todoItem.item.querySelector('span');
+
+            todosArray.push(inputText.textContent);
+            console.log('todosArray начало = ', todosArray);
+            updateLocalStorage(inkey, todosArray);
+
+            buttonAddRemove(todoItem, inkey, todosArray);
             todoItemForm.button.disabled = true;
             // обнуляем значение в поле, что бы не пришлось стирать врчную
             todoItemForm.input.value = '';
